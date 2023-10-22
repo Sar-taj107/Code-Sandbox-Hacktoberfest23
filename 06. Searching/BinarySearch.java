e80c6a4a99ec941addfa62133be4beead064b344
Pratap Pawar <87373605+pawarspeaks@users.noreply.github.com>
2023-10-23 03:05:59 +0530

    Update BinarySearch.java

--- 06. Searching/BinarySearch.java
+++ 06. Searching/BinarySearch.java
@@ -1,22 +1,36 @@
-import java.util.*;
+import java.util.Arrays;
 
 public class BinarySearch {
-    public static void main(String[] args) {
-        int[] arr= { 1,2,3,4,5,6,7,9,12,14,16,18,19,23};
-        int target = 12;
-        System.out.println("ELement found at index:"+binarySearch(arr,target));
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1; // Target not found
     }
-    public static int binarySearch(int nums[], int target)
-    {
-        int left=0;
-        int right= nums.length-1;
-        while(left<=right)
-        {
-            int mid=left+(right-left)/2;
-            if(nums[mid]==target)return mid;
-            else if(target>nums[mid]) left=mid+1;
-            else if(target<nums[mid]) right=mid-1;
-         }
-        return -1;
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int target = 5;
+
+        int result = binarySearch(arr, target);
+        if (result != -1) {
+            System.out.println("Element found at index " + result);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
     }
 }
