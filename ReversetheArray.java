6b4bea8e920c42c165252813c13a8d8bbcc0a5d6
Riya Sahu <106054091+riyusahu@users.noreply.github.com>
2023-10-25 18:58:57 +0530

    Add files via upload
    I am new to open source , this is my first contribution please merge it

---
+++ ReversetheArray.java
@@ -0,0 +1,11 @@
+public class ReversetheArray {
+    public static void main(String[] args) {
+        
+        int arr[] = {10,23,23,45,8};
+
+        for(int i = arr.length-1 ; i >= 0;i--){
+            System.out.println(arr[i]);
+        }
+    }
+    
+}
