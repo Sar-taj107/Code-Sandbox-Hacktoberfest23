64777a7428c31165306e307d28f9afe9f5c8c010
Md Tajuddin <89903283+Sar-taj107@users.noreply.github.com>
2024-02-14 19:35:09 +0530

    Update RearrangeArrayElementsbySign.java

--- Leetcode/Medium/2149. Rearrange Array Elements by Sign/RearrangeArrayElementsbySign.java
+++ Leetcode/Medium/2149. Rearrange Array Elements by Sign/RearrangeArrayElementsbySign.java
@@ -51,3 +51,60 @@ Enter the Element: -1 1
 After reaarange: 1,-1
 
 */
+
+----------------------------------------------java--------------------------------------------
+//2. approach using ArrayList<>()
+import java.util.*;
+public class RearrangeArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size: ");
+        int n=sc.nextInt();
+        int[] nums = new int[n];
+        System.out.print("Enter the Element: ");
+        for(int i=0; i<n; i++){
+            nums[i]=sc.nextInt();
+        }
+        int[] ans = rearrangeArray(nums);
+        System.out.print("After reaarange: ");
+        for(int i=0; i<n; i++){
+            System.out.print(ans[i]);
+            if(i<n-1){
+                System.out.print(",");
+            }
+        }
+    }
+    public static int[] rearrangeArray(int[] nums){
+        List<Integer> pNum = new ArrayList<>();
+        List<Integer> nNum = new ArrayList<>();
+        for(int num : nums){
+            if(num > 0){
+                pNum.add(num);
+            }else{
+                nNum.add(num);
+            }
+        }
+        int x=0, y=0;
+        for(int i=0; i<nums.length; i++){
+            if(i%2==0){
+                nums[i]=pNum.get(x++);
+            }else{
+                nums[i]=nNum.get(y++);
+            }
+        }
+        return nums;
+    }
+}
+
+/*
+Output: 
+
+Enter array size: 6
+Enter the Element: 3 1 -2 -5 2 -4
+After reaarange: 3,-2,1,-5,2,-4
+
+Enter array size: 2
+Enter the Element: -1 1
+After reaarange: 1,-1
+
+*/
