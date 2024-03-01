414f57e1982227bf66d1397e26013177c4c5a5e4
Md Tajuddin <89903283+Sar-taj107@users.noreply.github.com>
2024-03-01 20:29:44 +0530

    Update MaximumOddBinaryNumber.java

--- Leetcode/Easy/2864. Maximum Odd Binary Number/MaximumOddBinaryNumber.java
+++ Leetcode/Easy/2864. Maximum Odd Binary Number/MaximumOddBinaryNumber.java
@@ -39,4 +39,86 @@ Enter Binary string: 010110
 Maximum odd binary number: 110001
 
 */
+-----------------------------------------------------------------************----------------------------------------------------
+    
+//package Easy.2864. Maximum Odd Binary Number;
+//2.approach Tc- O(n), sc - O(n)
+import java.util.*;
+public class MaximumOddBinaryNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Binary string: ");
+        String s = sc.next();
+        String ans = maximumOddBinaryNumber(s);
+        System.out.print("Maximum odd binary number: "+ans);
+    }
+    public static String maximumOddBinaryNumber(String s){
+
+        int n=s.length();
+        StringBuilder sb = new StringBuilder();
+        long countOne = s.chars().filter(c->c=='1').count();
+
+        while(n-- > 1){
+            if(countOne-- > 1){
+                sb.append(1);
+            }else{
+                sb.append(0);
+            }
+        }
+        sb.append(1);
+        return sb.toString();
+    }
+}
+
+/*
+Output:
+
+Enter Binary string: 010
+Maximum odd binary number: 001
+
+Enter Binary string: 0101
+Maximum odd binary number: 1001
+
+Enter Binary string: 010110
+Maximum odd binary number: 110001
+
+*/
+----------------------------------------------------------------**********************--------------------------------------------------
+
+//package Easy.2864. Maximum Odd Binary Number;
+//3.approach Tc- O(nlogn) due to sorting, sc - O(n)
+import java.util.*;
+public class MaximumOddBinaryNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Binary string: ");
+        String s = sc.next();
+        String ans = maximumOddBinaryNumber(s);
+        System.out.print("Maximum odd binary number: "+ans);
+    }
+    public static String maximumOddBinaryNumber(String s){
+
+        int n=s.length();
+        char[] ch = s.toCharArray();
+        Arrays.sort(ch);
+        String str = new String(ch);
+        StringBuilder sb = new StringBuilder(str.substring(0,n-1));
+        sb.reverse();
+        return sb.append(1).toString();
+    }
+}
+
+/*
+Output:
+
+Enter Binary string: 010
+Maximum odd binary number: 001
+
+Enter Binary string: 0101
+Maximum odd binary number: 1001
+
+Enter Binary string: 010110
+Maximum odd binary number: 110001
+
+*/
 
