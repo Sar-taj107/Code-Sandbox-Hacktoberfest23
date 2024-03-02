512a10276397afd9a7494cc5c26436828dcb7ceb
Md Tajuddin <89903283+Sar-taj107@users.noreply.github.com>
2024-03-02 23:36:27 +0530

    Update SquaresofaSortedArray.java

--- Leetcode/Easy/977. Squares of a Sorted Array/SquaresofaSortedArray.java
+++ Leetcode/Easy/977. Squares of a Sorted Array/SquaresofaSortedArray.java
@@ -1,4 +1,5 @@
 //package Easy.977. Squares of a Sorted Array;
+//1. Approach optimize using Two - pointer TC - O(n) 
 import java.util.*;
 public class SquaresofaSortedArray {
     public static void main(String[] args) {
@@ -51,4 +52,48 @@ Enter the Element: -7 -3 2 3 11
 sorted square array: 4 9 9 49 121
 
 */
+------------------------------------------------------------*******************-----------------------------------------------------------
+//2. Approach using sorting TC- O(nlogn)
+//package Easy.977. Squares of a Sorted Array;
+import java.util.*;
+public class SquaresofaSortedArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size: ");
+        int n=sc.nextInt();
+        int[] nums = new int[n];
+        System.out.print("Enter the Element: ");
+        for(int i=0; i<n; i++){
+            nums[i] = sc.nextInt();
+        }
+        int[] ans = sortedSquares(nums);
+        System.err.print("sorted square array: ");
+        for(int i=0; i<n; i++){
+            System.err.print(ans[i]+" ");
+        }
 
+    }
+    public static int[] sortedSquares(int[] nums){
+        int idx = 0;
+        int[] res = new int[nums.length];
+        for(int num : nums){
+            res[idx++] = num * num;
+        } 
+        Arrays.sort(res);
+        return res;
+    } 
+}
+
+
+/*
+Output:
+
+Enter array size: 5
+Enter the Element: -4 -1 0 3 10
+sorted square array: 0 1 9 16 100
+
+Enter array size: 5
+Enter the Element: -7 -3 2 3 11
+sorted square array: 4 9 9 49 121
+
+*/
