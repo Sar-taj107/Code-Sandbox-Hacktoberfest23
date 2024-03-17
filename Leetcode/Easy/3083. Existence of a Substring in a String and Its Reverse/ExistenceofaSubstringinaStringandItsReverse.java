70d207428f487fcfb0efdf48c21abd57db3f14e4
Md Tajuddin <89903283+Sar-taj107@users.noreply.github.com>
2024-03-17 23:37:16 +0530

    Update ExistenceofaSubstringinaStringandItsReverse.java

--- Leetcode/Easy/3083. Existence of a Substring in a String and Its Reverse/ExistenceofaSubstringinaStringandItsReverse.java
+++ Leetcode/Easy/3083. Existence of a Substring in a String and Its Reverse/ExistenceofaSubstringinaStringandItsReverse.java
@@ -1,4 +1,6 @@
 //package Easy.3083. Existence of a Substring in a String and Its Reverse;
+//1.Approach linear sol 
+//Tc -O(n) Sc - O(1)
 import java.util.*;
 public class ExistenceofaSubstringinaStringandItsReverse {
     public static void main(String[] args) {
@@ -34,3 +36,29 @@ Enter the String: abcd
 false
 
 */
+
+---------------------------------------------------------********************************-----------------------------------------------------
+//2. approach using HashSet 
+//Tc -O(n^2) Sc- O(n)
+class Solution {
+    public boolean isSubstringPresent(String s) {
+        int n=s.length();
+        Set<String> st = new HashSet<>();
+        for(int i=0; i<n-1; i++){
+            int j=i+2;
+            String str = s.substring(i,j);
+            st.add(str);
+        }
+        StringBuilder sb = new StringBuilder(s);
+        sb.reverse();
+        String str1 = new String(sb);
+        for(int i=0; i<str1.length()-1; i++){
+            int j=i+2;
+            String s1 = str1.substring(i,j);
+            if(st.contains(s1)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
