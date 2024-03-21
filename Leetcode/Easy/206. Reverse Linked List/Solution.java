e67ad07ad20307eae6a972fb8bb51b9406c4294c
Md Tajuddin <89903283+Sar-taj107@users.noreply.github.com>
2024-03-21 18:34:15 +0530

    Update Solution.java

--- Leetcode/Easy/206. Reverse Linked List/Solution.java
+++ Leetcode/Easy/206. Reverse Linked List/Solution.java
@@ -10,3 +10,18 @@ class Solution {
         return reverse(next, head);
     }
 }
+----------------------------------------**************************************--------------------------------------
+//2. iterative approach
+class Solution {
+    public ListNode reverseList(ListNode head) {
+
+        ListNode prev = null. curr = head;
+        while(curr != null)
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
