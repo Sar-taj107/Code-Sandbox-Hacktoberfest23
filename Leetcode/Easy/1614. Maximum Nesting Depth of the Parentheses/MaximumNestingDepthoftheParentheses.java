10ae635b08616921344f815c40d9f28d5d2bd210
Md Tajuddin <89903283+Sar-taj107@users.noreply.github.com>
2024-04-04 17:22:53 +0530

    Update MaximumNestingDepthoftheParentheses.java

--- Leetcode/Easy/1614. Maximum Nesting Depth of the Parentheses/MaximumNestingDepthoftheParentheses.java
+++ Leetcode/Easy/1614. Maximum Nesting Depth of the Parentheses/MaximumNestingDepthoftheParentheses.java
@@ -1,4 +1,5 @@
 //package Easy.1614. Maximum Nesting Depth of the Parentheses;
+// approach 1 Tc - O(n) sc- O(1)
 import java.util.*;
 public class MaximumNestingDepthoftheParentheses {
     public static void main(String[] args) {
@@ -38,4 +39,31 @@ Enter the String: ()
 Maximum nesting depth: 1
  
 */
+------------------------------------------*****************************------------------------------------------------
+//approach 2 using stack 
+Tc- O(n) Sc- O(n) 
+import java.util.*;
+public class MaximumNestingDepthoftheParentheses {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the String: ");
+        String s = sc.nextLine();
+        int ans = maxDepth(s);
+        System.out.print("Maximum nesting depth: "+ans);
+    }
+    public static int maxDepth(String s){
 
+        int maxDepth = 0;
+        Stack<Character> st= new Stack<>();
+        char[] ch = s.toCharArray();
+        for(char c : ch){
+            if( c=='(' ){
+                st.push('(');
+                maxDepth = Math.max(maxDepth, st.size());
+            }else if( c==')' ){
+                st.pop();
+            }
+        }
+        return maxDepth;
+    }
+}
