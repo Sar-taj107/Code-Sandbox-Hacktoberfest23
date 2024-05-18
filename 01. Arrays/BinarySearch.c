e99198d3885ac5802dccd3713ce03794941cda5d
Taylor <taylorphelps44@gmail.com>
2024-05-18 17:40:30 -0400

    Updated Iterative and Recursive form for BinarySearch

--- 01. Arrays/BinarySearch.c
+++ 01. Arrays/BinarySearch.c
@@ -8,41 +8,40 @@ struct Array
     int length;
 };
 
-int binarySearch(struct Array arr, int key)
+int binarySearch(struct Array *arr, int key)
 {
-    int l=0, h=arr.length-1;
+    int l = 0, h = arr->length - 1;
     int mid;
-    while(l<=h)
+    while (l <= h)
     {
-        mid = (l+h)/2;
-        if(arr.A[mid]==key)
+        mid = (1 + h) / 2;
+        if(arr->A[mid] == key)
             return mid;
-        else if(arr.A[mid]<key)
-            l = mid+1;
-        else if(arr.A[mid]>key)
-            h = mid-1;
+        else if(arr->A[mid] < key)
+            l = mid + 1;
+        else 
+            h = mid - 1;
     }
     return -1;
 }
 
 //2. Recursive form
-int RBinarySearch(struct Array arr, int key, int l, int h)
+int RBinarySearch(struct Array *arr, int key, int l, int h)
 {
-    int mid = (l+h)/2;
-    if(l<=h)
-    {
-        if(arr.A[mid] == key)
+    if (l <= h) {
+        int mid = (1 + h) / 2;
+        if (arr->A[mid] == key)
             return mid;
-        else if(arr.A[mid]<key)
-            return RBinarySearch(arr, key, mid+1, h);
+        else if (arr->A[mid] < key)
+            return RBinarySearch(arr, key, mid + 1, h);
         else
-            return RBinarySearch(arr, key, l, mid-1);
+            return RBinarySearch(arr, key, l, mid - 1);
     }
     return -1;
 }
 int main()
 {
     struct Array arr={{4,8,10,15,18,21,24,27,29,33,34,37,39,41,43},15,15};
-    printf("Element found at index: %d",RBinarySearch(arr, 15, 0, arr.length-1));
+    printf("Element found at index: %d\n", binarySearch(&arr, 15));
     return 0;
 }
