e7ebb4edb9183a9f2500f36336d42c702a399cfb
Md Tajuddin <89903283+Sar-taj107@users.noreply.github.com>
2024-06-05 22:55:03 +0530

    Update LongestPalindrome.java

--- Leetcode/Easy/409. Longest Palindrome/LongestPalindrome.java
+++ Leetcode/Easy/409. Longest Palindrome/LongestPalindrome.java
@@ -32,3 +32,14 @@ public class LongestPalindrome {
         return len;
     }
 }
+
+/*
+Example 1:
+Input: s = "abccccdd"
+Output: 7
+Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
+
+Example 2:
+Input: s = "a"
+Output: 1
+*/
