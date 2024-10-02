79077fd5ffa4de76ae03f4ec4a61106ba2418355
Abhishek Tripathi <42455093+abhishektripathi66@users.noreply.github.com>
2024-10-02 14:16:53 +0530

    Create Solution.java

---
+++ Leetcode/Medium/1497. Check If Array Pairs Are Divisible by k/Solution.java
@@ -0,0 +1,32 @@
+/**
+1497. Check If Array Pairs Are Divisible by k
+Solved
+Medium
+Topics
+Companies
+Hint
+Given an array of integers arr of even length n and an integer k.
+We want to divide the array into exactly n / 2 pairs such that the sum of each pair is divisible by k.
+Return true If you can find a way to do that or false otherwise.
+**/
+class Soliution {
+    public boolean canArrange(int[] arr, int k) {
+        int [] freq = new int[k];
+        for(int num: arr){
+            int rem = num % k;
+            if(rem < 0){
+                rem = rem + k;
+            }
+            freq[rem]++;
+        }
+        if(freq[0] % 2 != 0){
+            return false;
+        }
+        for(int i = 1;i <= k / 2;i++){
+            if(freq[i] != freq[k-i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
